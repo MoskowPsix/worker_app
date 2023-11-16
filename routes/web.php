f6df897e6de1cdc1a6d9589b05c6dc7a5c8189e7
MoskowPsix <bolshe.kivi@gmail.com>
2023-11-16 18:24:35 +0500

    lesson 36 ok

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,10 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/workers',[\App\Http\Controllers\WorkerController::class, 'index'])->name('worker.index');
-Route::get('/workers/create',[\App\Http\Controllers\WorkerController::class, 'create'])->name('worker.create');
-Route::get('/workers/show/{id}',[\App\Http\Controllers\WorkerController::class, 'show'])->name('worker.show');
-Route::post('/workers',[\App\Http\Controllers\WorkerController::class, 'store'])->name('worker.store');
-Route::patch('/workers/{id}',[\App\Http\Controllers\WorkerController::class, 'update'] )->name('worker.update');
-Route::get('/workers/{id}/edit',[\App\Http\Controllers\WorkerController::class, 'edit'] )->name('worker.edit');
-Route::delete('/workers/{id}',[\App\Http\Controllers\WorkerController::class, 'delete'] )->name('worker.delete');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
